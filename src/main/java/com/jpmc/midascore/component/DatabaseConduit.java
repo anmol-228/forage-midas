7ea3c6b19b6042c8cb1a34704fbdddd68b61d59e
anmol-228 <trivedianmol277@gmail.com>
2026-03-12 19:04:24 +0530

    Implement Midas Core functionality: - Kafka listener for transaction processing - Transaction validation and persistence using H2 + JPA - Incentive API integration for transaction rewards - REST endpoint (/balance) for querying user balances

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,89 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
+
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DatabaseConduit {
-    private final UserRepository userRepository;
 
-    public DatabaseConduit(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
+
+    public DatabaseConduit(UserRepository userRepository,
+                           TransactionRepository transactionRepository,
+                           RestTemplate restTemplate) {
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplate;
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
     }
 
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
+    public void listen(Transaction transaction) {
+
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            return;
+        }
+
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:8080/incentive",
+                transaction,
+                Incentive.class
+        );
+
+        float incentiveAmount = 0;
+
+        if (incentive != null) {
+            incentiveAmount = incentive.getAmount();
+        }
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+
+        recipient.setBalance(
+                recipient.getBalance()
+                        + transaction.getAmount()
+                        + incentiveAmount
+        );
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        TransactionRecord record =
+                new TransactionRecord(
+                        sender,
+                        recipient,
+                        transaction.getAmount(),
+                        incentiveAmount
+                );
+
+        transactionRepository.save(record);
+
+        // Debug printing for TaskFour
+        if (recipient.getName().equals("wilbur")) {
+            System.out.println("Wilbur balance: " + recipient.getBalance());
+        }
+
+        if (sender.getName().equals("wilbur")) {
+            System.out.println("Wilbur balance: " + sender.getBalance());
+        }
+    }
 }
